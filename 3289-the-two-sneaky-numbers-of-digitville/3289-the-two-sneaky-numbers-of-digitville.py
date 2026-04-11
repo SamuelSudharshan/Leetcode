6c242c69545bfb993a8e75d8227007e8189fd17c
SamuelSudharshan <samuelsudharshan2005@gmail.com>
2026-04-11 15:10:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (93.63%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -1,5 +1,11 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        return multimode(nums)
-
+        '''return multimode(nums)'''
+        map={}
+        ls=[]
+        for i in nums:
+            map[i]=map.get(i,0)+1
+            if map[i] == 2:
+                ls.append(i)
+        return ls
         
